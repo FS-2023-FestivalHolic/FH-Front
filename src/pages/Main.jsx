83afc08271eca5e94f7dde01ccbcb26824116f5f
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-10-10 21:12:41 +0900

    design: banner ui 구현 (#2) - 메인 페이지의 banner ui 구현 - 배너 이미지 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import Navigation from "../shared/Navigation";
+import MainImg from "../assets/mainImg.png";
 
 const Main = () => {
   return(
     <Wrapper>
       <Navigation />
+      <MainBanner>
+        <img src={MainImg} alt="From Grain to Galss"/>
+      </MainBanner>
     </Wrapper>  
   );
 };
@@ -12,5 +16,9 @@ const Main = () => {
 const Wrapper = styled.div`
   max-width: 1075px;
   margin: 0 auto;
+`;
+
+const MainBanner = styled.div`
+  margin: 24px 0;
 `;
 export default Main;
