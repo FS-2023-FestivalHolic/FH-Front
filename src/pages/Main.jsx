1a8f179a60485b0b2796fd219fd2b20b4b6d84bb
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-10-11 00:29:53 +0900

    design: 맥주item 메인 페이지에 렌더링 (#2) - 임의의 beerData 사용 - 아이템 클릭시 상세 페이지로 이동

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import styled from "styled-components";
 import Navigation from "../shared/Navigation";
 import MainImg from "../assets/mainImg.png";
+import BeerItem from "../components/BeerItem";
+import { Link } from "react-router-dom";
 
 const Main = () => {
+  // 임시 맥주 데이터 사용
+  const beerData = [
+    {id: 1, image: require('../assets/FirstJuiceLarger.png'), name: '첫즙라거', like: 5, tags: ['달달함', '부드러움']},
+    {id: 2, image: require('../assets/LemonRamalade.png'), name: '레몬라말레이드', like: 22, tags: ['달달함', '부드러움']},
+    {id: 3, image: require('../assets/LargerOnTheBeach.png'), name: '라거 온 더 비치', like: 5, tags: ['달달함', '부드러움']},
+    {id: 4, image: require('../assets/DarkLarger.png'), name: '다크 라거', like: 22, tags: ['달달함', '부드러움', '쓴맛']},
+  ]; 
+  const [data, setData] = useState(beerData);
   return(
     <Wrapper>
       <Navigation />
       <MainBanner>
         <img src={MainImg} alt="From Grain to Galss"/>
       </MainBanner>
+      <List>
+        {/*아이템 렌더링 */}
+        {data.map((beer, index) => (
+          <Link to={`/beer/${beer.id}`}>
+            <BeerItem key={index} item={beer} />
+          </Link>
+        ))}
+      </List>
     </Wrapper>  
   );
 };
@@ -21,4 +40,10 @@ const Wrapper = styled.div`
 const MainBanner = styled.div`
   margin: 24px 0;
 `;
+
+const List = styled.div`
+  max-width: 100%;
+  justify-content:space-between;
+  display: flex;
+`
 export default Main;
