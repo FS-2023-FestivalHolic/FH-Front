f6d224ce7c73f865c4a4e706707a4f137f4717cb
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-10-12 01:58:58 +0900

    design: Globalstyle 수정 (#5) - color props 삭제 - 마이페이지 배경색을 props로 변경할 수 있도록 수정

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -1,13 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-/* textColor가 보통은 회색인데 가끔 다른 색상 쓰일 때는 <GlobalStyle textColor="#000" /> 형식으로 사용할 수 있습니다=*/
     body {
         margin: 0 auto;
         padding: 0;
-        background-color: #FFF;
+        background-color:${props => props.specialPage ? '#ECECEC' : '#FFF'};
         font-family: font-family: Arial, Helvetica, sans-serif;
-        color: ${({ textColor }) => textColor || "#666"};
+        color: "#666";
     }
 `;
 
