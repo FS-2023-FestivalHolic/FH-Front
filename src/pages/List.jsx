9aa91b2cef37f99bd7e7d21ff1823726dcf7ca07
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-11-02 03:08:58 +0900

    design: 해시태그 필터 구현 (#6) - HashtagBox 컴포넌트 구현 - 해시태그 목록 생성 - 해시태그 선택, 삭제 기능 구현

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,5 +1,59 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import HashtagBox from "../components/HashtagBox";
+
 const List = () => {
-    return <div>리스트 페이지입니다.</div>;
+  const tags = ["#달달함", "#부드러움", "#상큼함", "#깔끔함", "#쓴맛", "#쌉쌀함", "#과일향", "#새콤달콤"];
+  const [selectTags, setSelectTags] = useState([]);
+
+  const addHashtag = (tag) => {
+    if(!selectTags.includes(tag)) {
+      setSelectTags([...selectTags, tag]);
+    }
   };
-  
-  export default List;
+
+  const removeTag = (tag) => {
+    setSelectTags(selectTags.filter((selectTag) => selectTag !== tag));
+  }; 
+
+  return (
+    <Wrapper> 
+      <HashtagsContainer>
+        {tags.map((tag, index) => (
+          <div>
+            <HashTag key={index} onClick={() => addHashtag(tag)}>
+              {tag}
+            </HashTag>
+            {index < tags.length - 1 && <span>|</span>}
+          </div>
+        ))}
+      </HashtagsContainer>
+
+      <HashtagBox tags={selectTags} removeTag={removeTag} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  max-width: 1075px;
+  margin: 0 auto;
+`;
+
+const HashtagsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap:15px;
+  color: #666;
+  font-size: 15px;
+  font-weight: 400;
+  margin-top: 30px;
+`;
+
+const HashTag = styled.span`
+  padding: 5px;
+  cursor: pointer;
+  margin-right: 15px;
+`; 
+
+
+export default List;
