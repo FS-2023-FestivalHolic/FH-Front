6e6900eb0f825bef03365ec53125773ed907032e
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-11-02 17:21:18 +0900

    design: 아이템 리스트 추가 (#6) - 4열로 아이템 추가 - 각 아이템에 상세페이지 링크 연결

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import HashtagBox from "../components/HashtagBox";
+import BeerItem from "../components/BeerItem";
+import { Link } from "react-router-dom";
 
 const List = () => {
   const tags = ["#달달함", "#부드러움", "#상큼함", "#깔끔함", "#쓴맛", "#쌉쌀함", "#과일향", "#새콤달콤"];
@@ -15,6 +17,22 @@ const List = () => {
   const removeTag = (tag) => {
     setSelectTags(selectTags.filter((selectTag) => selectTag !== tag));
   }; 
+
+  // 임시 맥주 데이터 사용
+  const beerData = [
+    {id: 1, image: require('../assets/FirstJuiceLarger.png'), name: '첫즙라거', like: 5, tags: ['달달함', '부드러움']},
+    {id: 2, image: require('../assets/LemonRamalade.png'), name: '레몬라말레이드', like: 22, tags: ['달달함', '부드러움']},
+    {id: 3, image: require('../assets/LargerOnTheBeach.png'), name: '라거 온 더 비치', like: 5, tags: ['달달함', '부드러움']},
+    {id: 4, image: require('../assets/DarkLarger.png'), name: '다크 라거', like: 22, tags: ['달달함', '부드러움', '쓴맛']},
+    {id: 5, image: require('../assets/LemonRamalade.png'), name: '레몬라말레이드', like: 22, tags: ['달달함', '부드러움']},
+    {id: 6, image: require('../assets/LargerOnTheBeach.png'), name: '라거 온 더 비치', like: 5, tags: ['달달함', '부드러움']},
+    {id: 7, image: require('../assets/DarkLarger.png'), name: '다크 라거', like: 22, tags: ['달달함', '부드러움', '쓴맛']},
+    {id: 8, image: require('../assets/LemonRamalade.png'), name: '레몬라말레이드', like: 22, tags: ['달달함', '부드러움']},
+    {id: 9, image: require('../assets/LargerOnTheBeach.png'), name: '라거 온 더 비치', like: 5, tags: ['달달함', '부드러움']},
+    {id: 10, image: require('../assets/DarkLarger.png'), name: '다크 라거', like: 22, tags: ['달달함', '부드러움', '쓴맛']},
+    {id: 11, image: require('../assets/LemonRamalade.png'), name: '레몬라말레이드', like: 22, tags: ['달달함', '부드러움']},
+    {id: 12, image: require('../assets/LargerOnTheBeach.png'), name: '라거 온 더 비치', like: 5, tags: ['달달함', '부드러움']},
+  ]; 
 
   return (
     <Wrapper> 
@@ -36,6 +54,14 @@ const List = () => {
         <Bar>|</Bar>
         <SortButton>좋아요순</SortButton>
       </SortOptions>
+
+      <ItemContainer>
+        {beerData.map((beer, index) => (
+          <Link to={`/beer/${beer.id}`} style={{ textDecoration: "none"}}>
+            <BeerItem key={index} item={beer} />
+          </Link>
+        ))}
+      </ItemContainer>
     </Wrapper>
   );
 };
@@ -79,6 +105,14 @@ const SortButton = styled.button`
 const Bar = styled.span`
   color: #666666B0;
   margin: 0 12px;
+`;
+
+const ItemContainer = styled.div`
+  max-width: 100%;
+  justify-content:space-between;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-gap: 9px;
 `;
 
 export default List;
